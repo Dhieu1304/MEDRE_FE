6bf57ff29d13f56e633d81e672bca0372c9871fb
Dhieu1304 <d.hieu.13.04@gmail.com>
2023-02-10 23:52:53 +0700

    [main][config]: update config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "airbnb",
-        "plugin:prettier/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    extends: ["eslint:recommended", "plugin:react/recommended", "airbnb", "plugin:prettier/recommended"],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
-  }
-}
+    plugins: ["react"],
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }]
+    }
+};
