8097a91daa6b8776e952717c67611ae1b6fbee92
Dhieu1304 <d.hieu.13.04@gmail.com>
2023-02-12 21:47:19 +0700

    fix eslint, prettier, precomit husky

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getItem, LOCAL_STORAGE } from "../utils/localStorage";
+import { getItem, LOCAL_STORAGE } from "../utils/localStorageUtil";
 
 const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_BE_URL,
