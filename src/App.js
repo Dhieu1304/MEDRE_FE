413a4eddd96ee13ddc84c930979378ecadf876f2
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2023-02-13 17:44:47 +0700

    [sang][component]: GlobalStyles, add images for assets

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import logo from "./assets/images/logo.svg";
 import "./App.css";
-// import localStorageUtil from "./utils/localStorageUtil";
+import images from "./assets/images";
 
 function App() {
-  const testCode = () => {
-    // console.log("Local: ", localStorageUtil.LOCAL_STORAGE);
-    return 1;
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={images.logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-
-        <button onClick={testCode} type="submit">
-          test code
-        </button>
       </header>
     </div>
   );
