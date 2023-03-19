2e0ce246464a56b470cf0ebc76d1897f1763743b
Đỗ Xuân Thanh <thanhcr2001@gmail.com>
2023-03-19 17:19:42 +0700

    Theme and Language
    Add switch button

--- src/features/auth/ChangePassword.js
+++ src/features/auth/ChangePassword.js
@@ -1,5 +1,35 @@
-function ChangePassword() {
-  return <div>ChangePassword 123456</div>;
-}
+import * as React from "react";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import Switch from "@mui/material/Switch";
 
-export default ChangePassword;
+export default function ChangePassword() {
+  const [theme, setTheme] = React.useState(false);
+  const [language, setLanguage] = React.useState(false);
+  const [dataTheme, setDataTheme] = React.useState("Light");
+  const [dataLanguage, setDataLanguage] = React.useState("ENG");
+  const themeChange = () => {
+    setTheme(!theme);
+    if (theme == true) setDataTheme("Dark");
+    else setDataTheme("Light");
+  };
+  const languageChange = () => {
+    setLanguage(!language);
+    if (language == true) setDataLanguage("VIE");
+    else setDataLanguage("ENG");
+  };
+  return (
+    <FormControl component="fieldset" variant="standard">
+      <FormGroup>
+        <FormControlLabel control={<Switch checked={theme} onChange={themeChange} name="theme" />} label={dataTheme} />
+        <FormControlLabel
+          control={<Switch checked={language} onChange={languageChange} name="language" />}
+          label={dataLanguage}
+        />
+      </FormGroup>
+    </FormControl>
+  );
+}
