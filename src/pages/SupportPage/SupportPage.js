b56ba91980218718a434a39f3fe9af475d882b31
Đỗ Xuân Thanh <thanhcr2001@gmail.com>
2023-07-02 16:41:45 +0700

    Create support detail
    Support detail for user

--- src/pages/SupportPage/SupportPage.js
+++ src/pages/SupportPage/SupportPage.js
@@ -1,12 +1,16 @@
 import { useTranslation } from "react-i18next";
 import { useAuthStore } from "../../store/AuthStore";
-
+import images from "../../assets/images";
+import "./SupportPage.css";
+import { Avatar, Button, TextField } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import { useState } from "react";
 export default function SupportPage() {
   const authStore = useAuthStore();
   const { t } = useTranslation("supportPage");
   return (
-    <div>
-      {t("title")} - {authStore.user?.name}
-    </div>
+    <>
+      <div>{t("title")}</div>
+    </>
   );
 }
