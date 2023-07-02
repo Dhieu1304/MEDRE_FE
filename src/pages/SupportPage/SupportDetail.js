54d40d7f6fc12909f1d108aa711fe06a03b43097
Đỗ Xuân Thanh <thanhcr2001@gmail.com>
2023-07-02 16:42:59 +0700

    Update SupportDetail.js
    Change the classname and class-element

--- src/pages/SupportPage/SupportDetail.js
+++ src/pages/SupportPage/SupportDetail.js
@@ -5,7 +5,7 @@ import "./SupportPage.css";
 import { Avatar, Button, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { useState } from "react";
-export default function SupportPage() {
+export default function SupportDetail() {
   const authStore = useAuthStore();
   const { t } = useTranslation("supportPage");
   function createData(avatar, name, description) {
